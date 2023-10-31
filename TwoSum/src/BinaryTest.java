b85dde2a7c324eb69cd32f34954ffaf50a4fc65e
hyeyeonkim <hkim7963@gmail.com>
2023-10-31 03:23:50 -0700

    My First JAVA SE

---
+++ TwoSum/src/BinaryTest.java
@@ -0,0 +1,12 @@
+public class BinaryTest {
+    public static void main(String[] args) {
+        int a=10;
+        float f=35.6f;
+        boolean b=false;
+        char c='A';
+        String s="APPLE";
+        int decimal=69;
+//        69를 10진수, 2진수, 8진수, 16진수로!
+        System.out.println("decimal = " + decimal);
+    }
+}
---
+++ TwoSum/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args){
+        int a,b,sum;
+        a = 1;
+        b = 1000;
+        sum = a+b;
+        System.out.println(sum);
+    }
+}
