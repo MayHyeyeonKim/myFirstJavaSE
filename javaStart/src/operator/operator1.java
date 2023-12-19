0545265d1e110f869b96cfc8f2e84d060cac09f5
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-18 20:37:54 -0800

    javaStart: Inflearn - instructor:Younghan Kim

---
+++ javaStart/src/operator/operator1.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class operator1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
