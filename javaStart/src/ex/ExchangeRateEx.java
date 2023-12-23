dd64137b5e64630c6197786a8a258bae07b45134
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-22 21:47:19 -0800

    Arrogant ExchangeRateEx.java

---
+++ javaStart/src/ex/ExchangeRateEx.java
@@ -0,0 +1,16 @@
+package ex;
+
+public class ExchangeRateEx {
+    public static void main(String[] args) {
+        int dollar = -20;
+
+        if(dollar<0){
+            System.out.println("Are you kidding me!");
+        } else if(dollar == 0){
+            System.out.println("You do not hava any money to exchange.");
+        } else {
+            int won = dollar * 1300;
+            System.out.println("The amount to exchange is: " + won + " KRW");
+        }
+    }
+}
