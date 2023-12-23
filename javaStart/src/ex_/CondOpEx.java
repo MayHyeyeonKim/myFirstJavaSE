0063b025ca46aefcb3b9b81cf6f7acfbcd8d5975
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-22 23:02:08 -0800

    ternary operator

---
+++ javaStart/src/ex_/CondOpEx.java
@@ -0,0 +1,13 @@
+package ex_;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 30;
+
+        int max = (firstNum > secondNum) ? firstNum : secondNum;
+        System.out.println("max number is: " + max);
+    }
+
+
+}
