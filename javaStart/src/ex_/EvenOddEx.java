bf69788158fc1e3bed6b3a0e5cd06f33fbdf6ef4
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-22 23:07:14 -0800

    ternary operator Even or Odd

---
+++ javaStart/src/ex_/EvenOddEx.java
@@ -0,0 +1,10 @@
+package ex_;
+
+public class EvenOddEx {
+    public static void main(String[] args) {
+        int x = 3;
+        String result = (x % 2 == 0) ? "Even" : "Odd";
+        System.out.println("x = " + x + ", " + result);
+    }
+    
+}
