b849708945d4a0cf5f8a62c044a7522138dea1b0
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-23 11:00:00 +0000

    while & do While

---
+++ javaStart/src/loop/DoWhile1_1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class DoWhile1_1 {
+    public static void main(String[] args) {
+        int i = 10;
+        do{
+            System.out.println("current: " + i);
+            i++;
+        } while(i<3);
+    }
+}
