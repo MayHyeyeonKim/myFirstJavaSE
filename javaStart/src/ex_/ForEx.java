b8833afb72f00fc83aec77be63f148b63101b96a
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-24 21:38:53 -0800

    while vs for

---
+++ javaStart/src/ex_/ForEx.java
@@ -0,0 +1,9 @@
+package ex_;
+
+public class ForEx {
+    public static void main(String[] args) {
+        for(int count=1; count <= 10; count++){
+            System.out.println(count);
+        }
+    }
+}
